eb7ae08a1b66a1e6ede6d1c42127f54e95a99ef3
mariana-santos <marianasfernandessousa@gmail.com>
2023-05-31 22:43:39 -0300

    area do voluntario finalizada

--- RWD/gs2-next/app/Components/AreaVoluntario/index.js
+++ RWD/gs2-next/app/Components/AreaVoluntario/index.js
@@ -1,186 +1,177 @@
 import { useState } from "react"
 
-import Campo from "../Campo"
+// import Campo from "../Campo"
 
 import validator from "validator"
-
-import { HiOutlineEnvelope, HiOutlineIdentification } from 'react-icons/hi2'
-import { RiLockPasswordLine } from 'react-icons/ri'
-import { RxPerson } from 'react-icons/rx'
-import { BsTelephone } from 'react-icons/bs'
-
-import { PatternFormat } from 'react-number-format';
 
 import '../../styles/form.css'
 
 import './style.css'
 
-import { Switch, FormControlLabel } from "@mui/material"
+import { Switch, FormControlLabel, } from "@mui/material"
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+import Moment from "react-moment"
+import Campo from "../Campo"
+import { BsCalendarDate } from "react-icons/bs"
+
+import moment from "moment"
+import { ToastContainer, toast } from "react-toastify"
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function AreaVoluntario() {
-    const [nome, setNome] = useState('Mariana Santos')
-    const [errorNome, setErrorNome] = useState(null)
-
-    const [email, setEmail] = useState('mariana@gmail.com')
-    const [errorEmail, setErrorEmail] = useState(null)
-
-    const [senha, setSenha] = useState('senha123')
-    const [errorSenha, setErrorSenha] = useState(null)
-
-    const [cpf, setCpf] = useState('')
-    const [errorCpf, setErrorCpf] = useState(null)
-
-    const [celular, setCelular] = useState('(11) 9 5042-6440')
-    const [errorCel, setErrorCel] = useState(null)
 
     const [carregando, setCarregando] = useState(false)
+
+    const [habilitado, setHabilitado] = useState(false)
+
+    const hoje = new Date();
+    const amanha = new Date(hoje.getTime() + 24 * 60 * 60 * 1000);
+    const [data, setData] = useState(amanha);
+
+    const [turno, setTurno] = useState('Manhã');
 
     function handleSubmit(e) {
 
         e.preventDefault();
 
-        if (validaEmail() && validaSenha()) {
-            // setCarregando(true)
+        // if (validaEmail() && validaSenha()) {
+        // setCarregando(true)
 
-            // fetch(`http://localhost:8080/InvestiumAPI/rest/usuario/${email}/${senha}`)
-            //   .then((resp) => resp.json())
-            //   .then((data) => {
-            //     setCarregando(false)
-            //     if (data.nome && data.email && data.senha) {
-            //       toast.success('Usuário autenticado! Aguarde para ser direcionado.')
-            //       const dadosString = JSON.stringify(data);
-            //       sessionStorage.setItem("dadosUsuario", dadosString);
-            //       setUser(data)
-            //       setTimeout(() => {
-            //         window.location.href = '/perfil'
-            //       }, 2000)
-            //     } else {
-            //       toast.error('Email ou senha incorretos.')
-            //     }
-            //   })
-            //   .catch((error) => {
-            //     console.error(error)
-            //     setCarregando(false)
-            //   });
+        // fetch(`http://localhost:8080/InvestiumAPI/rest/usuario/${email}/${senha}`)
+        //   .then((resp) => resp.json())
+        //   .then((data) => {
+        //     setCarregando(false)
+        //     if (data.nome && data.email && data.senha) {
+        //       toast.success('Usuário autenticado! Aguarde para ser direcionado.')
+        //       const dadosString = JSON.stringify(data);
+        //       sessionStorage.setItem("dadosUsuario", dadosString);
+        //       setUser(data)
+        //       setTimeout(() => {
+        //         window.location.href = '/perfil'
+        //       }, 2000)
+        //     } else {
+        //       toast.error('Email ou senha incorretos.')
+        //     }
+        //   })
+        //   .catch((error) => {
+        //     console.error(error)
+        //     setCarregando(false)
+        //   });
+        // }
+
+        console.log(data)
+    }
+
+    function handleData(data) {
+        if (data <= new Date()) {
+            handleDataAgendamento()
+            // Futuramente esse erro só vai aparecer caso seja um novo agendamento: 
+            // ou seja, se o dia selecionado não tiver nenhum agendamento cadastrado 
+            // (que é quando o formulário de novo agendamento é mostrado)
+            toast.error("A data não pode ser hoje ou dias anteriores!")
+        }
+        else setData(data)
+    }
+
+    function handleDataAgendamento(e){
+        if (data <= new Date()) {
+            
         }
     }
 
-    function validaEmail() {
-
-        //se for válido e não for vazio retorna true e remove o erro
-        if (email !== '' && email !== null && validator.isEmail(email)) {
-            setErrorEmail(null)
-            return true
-        }
-
-        //se cair aqui tem algo errado e entram as validações especificas
-        else {
-            if (email === '' || email === null || email === undefined) {
-                setErrorEmail('Email é obrigatório!')
-                return false
-            }
-
-            if (!validator.isEmail(email)) {
-                setErrorEmail('Email inválido!')
-                return false
-            }
-        }
-
-    }
-
-    function validaSenha() {
-        if (senha !== '' && senha !== null) {
-            setErrorSenha(null)
-            return true
-        }
-        else {
-            setErrorSenha('Senha é obrigatória!')
-            return false
-        }
-    }
+    function handleSelecaoChange (e) {
+        setTurno(e.target.value);
+    };
 
     return (
         <form onSubmit={handleSubmit}>
+            <ToastContainer 
+                position="bottom-right"
+                autoClose={2000}
+                // hideProgressBar={false}
+                // newestOnTop={false}
+                closeOnClick
+                // rtl={false}
+                // pauseOnFocusLoss
+                // draggable
+                pauseOnHover
+                // theme="light"
+            />
             <div className="row-heading">
                 <div className="heading">
-                    <h2>Olá, Mariana!</h2>
-                    <small>Bem vindo(a) à área de voluntário! Aqui você pode agendar suas visitas para contribuição da nossa horta solidária.</small>
+                    <h2>Área do voluntário</h2>
+                    <small>Bem vindo(a) à área do voluntário! Aqui você pode agendar suas visitas para contribuição da nossa horta solidária.</small>
                 </div>
 
                 <div className="wrap-switch">
                     <FormControlLabel
                         value="habilitado"
-                        control={<Switch color="primary" />}
-                        label="Habilitar área do voluntário"
+                        control={<Switch value={habilitado} onChange={(e) => setHabilitado(e.target.checked)} />}
+                        label={`${habilitado ? 'Desabilitar' : 'Habilitar'} área do voluntário`}
                         labelPlacement="start"
                     />
                 </div>
             </div>
 
+            <div className="row-body">
+                { !habilitado && <div className="mask"> 
+                    Para agendar um trabalho voluntário, habilite a área do volutário no botão acima
+                </div> }
+                
+                <Calendar
+                    value={data}
+                    onChange={(data) => handleData(data)}
+                    className="calendario"
+                />
 
-            <Campo
-                label="Nome"
-                id="nome"
-                placeholder="Digite seu nome"
-                type="text"
-                icon={<RxPerson />}
-                errorMsg={errorNome}
-                value={nome}
-                onChange={e => setNome(e.target.value)}
-            />
+                <div className="agendamento">
 
-            <Campo
-                label="Email"
-                id="email"
-                placeholder="Digite seu email"
-                type="email"
-                icon={<HiOutlineEnvelope />}
-                errorMsg={errorEmail}
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-            />
+                    {/* Se tiver um dia selecionado já tiver agendamento, vai ser esse bloco */}
+                    <div className="agendamento-existente">
+                        <h3>Agendamento no dia selecionado: </h3>
+                        <div className="row">
+                            <p><strong>Dia:</strong> <Moment format="DD/MM/YYYY">{data}</Moment></p>
+                            <p><strong>Turno:</strong> Tarde</p>
+                        </div>
+                    </div>
 
-            <Campo
-                label="Celular"
-                id="celular"
-                placeholder="Digite seu celular"
-                type="text"
-                icon={<BsTelephone />}
-                errorMsg={errorCel}
-                value={celular}
-                onChange={e => setCelular(e.target.value)}
-                temMask
-            >
-                <PatternFormat format="(##) 9 #### ####" allowEmptyFormatting mask="_" defaultValue="(11) 9 5042 6440" />
-            </Campo>
+                    {/* Esse bloco só aparece se for um dia futuro e ainda não existir nenhum agendamento no dia */}
+                    <div className="novo-agendamento">
+                        <h3>Novo agendamento: </h3>
 
-            <Campo
-                label="CPF"
-                id="cpf"
-                placeholder="Digite seu CPF"
-                type="text"
-                icon={<HiOutlineIdentification />}
-                errorMsg={errorCpf}
-                value={cpf}
-                temMask
-                onChange={e => setCpf(e.target.value)}
-            >
-                <PatternFormat format="###.###.###-##" allowEmptyFormatting mask="_" />
-            </Campo>
+                        <p>Selecione a data no calendário ao lado:</p>
+                        <Campo 
+                            type="text"
+                            icon={<BsCalendarDate />}
+                            disabled
+                            value={moment(data).format("DD/MM/YYYY")}
+                            onChange={(e) => handleDataAgendamento(e)}
+                        />
 
-            <Campo
-                label="Senha"
-                id="senha"
-                placeholder="Digite sua senha"
-                type="password"
-                icon={<RiLockPasswordLine />}
-                errorMsg={errorSenha}
-                value={senha}
-                onChange={e => setSenha(e.target.value)}
-            />
+                        <p>Selecione o turno: </p>
 
-            <button type="submit" className="btn btn_primary arrow">Salvar</button>
+                        <div className="horarios">
+                            <label className="horario" htmlFor="manha">
+                                Manhã
+                                <input type="radio" name="turno" value="Manhã" id="manha" defaultChecked onChange={handleSelecaoChange} />
+                            </label>
 
+                            <label className="horario" htmlFor="tarde">
+                                Tarde
+                                <input type="radio" name="turno" value="Tarde" id="tarde" defaultChecked onChange={handleSelecaoChange} />
+                            </label>
+                        </div>
+
+                        
+                        <button type="submit" className="btn">Salvar</button>
+                    </div>
+                    
+                </div>
+            </div>
         </form>
     )
 }
