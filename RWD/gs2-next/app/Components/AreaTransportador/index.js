236a879bf4cf3fccc088050704b806459266468c
mariana-santos <marianasfernandessousa@gmail.com>
2023-06-01 23:27:48 -0300

    avanços na área do transportador

--- RWD/gs2-next/app/Components/AreaTransportador/index.js
+++ RWD/gs2-next/app/Components/AreaTransportador/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // import Campo from "../Campo"
 
@@ -8,33 +8,48 @@ import '../../styles/form.css'
 
 import './style.css'
 
-import { Switch, FormControlLabel, } from "@mui/material"
+import { Switch, FormControlLabel, Accordion, AccordionSummary, AccordionDetails, } from "@mui/material"
 
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-import Moment from "react-moment"
 import Campo from "../Campo"
 import { GiWeight } from "react-icons/gi"
 
-import moment from "moment"
 import { ToastContainer, toast } from "react-toastify"
 
 import 'react-toastify/dist/ReactToastify.css';
+import { PatternFormat } from "react-number-format"
+
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 export default function AreaTransportador() {
 
     const [carregando, setCarregando] = useState(false)
 
-    const [habilitado, setHabilitado] = useState(false)
-
-    const [carga, setCarga] = useState(0);
+    const [carga, setCarga] = useState(1);
 
     const [cep, setCep] = useState('');
     const cidade = useState('São Paulo');
     const [logradouro, setLogradouro] = useState('')
     const [numero, setNumero] = useState('')
     const [complemento, setComplemento] = useState('')
+
+    useEffect(() => {
+        if (cep.length == 8) {
+            const cep_formatado = cep.replace('-', '')
+            fetch(`https://viacep.com.br/ws/${cep_formatado}/json/`)
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data)
+                    setLogradouro(data.logradouro)
+                    setComplemento(data.complemento)
+                })
+                .catch((error) => {
+                    console.error(error)
+                });
+        }
+    }, [cep])
 
     function handleSubmit(e) {
 
@@ -65,29 +80,25 @@ export default function AreaTransportador() {
         //   });
         // }
 
-        console.log(data)
+        // console.log(data)
     }
 
-    function handleData(data) {
-        if (data <= new Date()) {
-            handleDataAgendamento()
-            // Futuramente esse erro só vai aparecer caso seja um novo agendamento: 
-            // ou seja, se o dia selecionado não tiver nenhum agendamento cadastrado 
-            // (que é quando o formulário de novo agendamento é mostrado)
-            toast.error("A data não pode ser hoje ou dias anteriores!")
+    const destinos = [
+        {
+            id_destino: 1,
+            endereco_destino: 'Rua Hipódromo, 720 CEP: 030.51-000',
+            responsavel_destino: 'Bruna Menegatti Vienna',
+            cel_destino: '(11) 95252-6565',
+            qtd_dependentes_destino: 5
+        },
+        {
+            id_destino: 2,
+            endereco_destino: 'Rua Armando Mamede Júnior, 149A CEP: 081.50-070 ',
+            responsavel_destino: 'Bruna Menegatti Vienna',
+            cel_destino: '(11) 95252-6565',
+            qtd_dependentes_destino: 2
         }
-        else setData(data)
-    }
-
-    function handleDataAgendamento(e) {
-        if (data <= new Date()) {
-
-        }
-    }
-
-    function handleSelecaoChange(e) {
-        setTurno(e.target.value);
-    };
+    ]
 
     return (
         <form onSubmit={handleSubmit} id="transporte">
@@ -97,79 +108,133 @@ export default function AreaTransportador() {
                 closeOnClick
                 pauseOnHover
             />
-            <div className="row-heading">
-                <div className="heading">
-                    <h2>Área do transportador</h2>
-                    <small>Bem vindo(a) à área do transportador! Aqui você pode cadastrar os dados do seu veículo e das colheitas disponíveis para entrega.</small>
-                </div>
 
-                <div className="wrap-switch">
-                    <FormControlLabel
-                        value="habilitado"
-                        control={<Switch value={habilitado} onChange={(e) => setHabilitado(e.target.checked)} />}
-                        label={`${habilitado ? 'Desabilitar' : 'Habilitar'} área do transportador`}
-                        labelPlacement="start"
-                    />
-                </div>
+            <div className="heading">
+                <h2>Área do transportador</h2>
+                <small>Bem vindo(a) à área do transportador! Aqui você pode cadastrar os dados do seu veículo e das colheitas disponíveis para entrega.</small>
             </div>
 
             <div className="row-body">
-                {!habilitado && <div className="mask">
-                    Para transportar uma nova colheita a algum de nossos destinos, habilite a área do transportador no botão acima
-                </div>}
 
                 <div className="transporte">
 
                     {/* Se a tabela transporte ainda não existir pra esse usuário, vai ser mostrado esse bloco pra cadastrar os dados iniciais da tabela */}
-                    <div className="novo-agendamento">
-                        <h3>Dados iniciais: </h3>
+                    {false ? (
+                        <div className="novo-agendamento">
+                            <h3>Dados iniciais: </h3>
 
-                        <p>Endereço:</p>
-                        <small>É importante que seu endereço seja na cidade de <strong>São Paulo</strong>, que é onde nossa horta é realizada</small>
+                            <p>Endereço:</p>
+                            <small>É importante que seu endereço seja na cidade de <strong>São Paulo</strong>, que é onde nossa horta é realizada</small>
 
-                        <Campo
-                            type="text"
-                            value={cep}
-                            onChange={(e) => setCep(e.target.value)}
-                            label="CEP"
-                            placeholder="Digite seu CEP"
-                        />
+                            <Campo
+                                type="text"
+                                value={cep}
+                                label="CEP"
+                                placeholder="Digite seu CEP"
+                                temMask
+                            >
+                                <PatternFormat
+                                    format="##.###-###"
+                                    allowEmptyFormatting mask="_"
+                                    onValueChange={(cep) => setCep(cep.value)}
+                                />
+                            </Campo>
 
-                        <Campo
-                            type="text"
-                            value={logradouro}
-                            onChange={(e) => setLogradouro(e.target.value)}
-                            label="Logradouro (rua)"
-                            placeholder="Digite seu logradouro"
-                        />
+                            <Campo
+                                type="text"
+                                value={logradouro}
+                                onChange={(e) => setLogradouro(e.target.value)}
+                                label="Logradouro (rua)"
+                                placeholder="Digite seu logradouro"
+                            />
 
-                        <Campo
-                            type="text"
-                            value={numero}
-                            onChange={(e) => setNumero(e.target.value)}
-                            label="Número"
-                            placeholder="Digite seu número"
-                        />
+                            <Campo
+                                type="text"
+                                value={numero}
+                                onChange={(e) => setNumero(e.target.value)}
+                                label="Número"
+                                placeholder="Digite seu número"
+                            />
 
-                        <Campo
-                            type="text"
-                            value={complemento}
-                            onChange={(e) => setComplemento(e.target.value)}
-                            label="Complemento"
-                            placeholder="Digite seu complemento"
-                        />
+                            <Campo
+                                type="text"
+                                value={complemento}
+                                onChange={(e) => setComplemento(e.target.value)}
+                                label="Complemento"
+                                placeholder="Digite seu complemento"
+                            />
 
-                        <p>Capacidade de carga do veículo:</p>
-                        <Campo
-                            type="number"
-                            icon={<GiWeight />}
-                            value={carga}
-                            onChange={(e) => setCarga(e.target.value)}
-                            min={1}
-                        />
+                            <p>Capacidade de carga do veículo:</p>
+                            <Campo
+                                type="number"
+                                icon={<GiWeight />}
+                                value={carga}
+                                onChange={(e) => setCarga(e.target.value)}
+                                min={1}
+                            />
 
-                        <button type="submit" className="btn">Salvar</button>
-                    </div>
+                            <button type="submit" className="btn">Salvar</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3>Destinos transportados atualmente: </h3>
+                            <small>Esses são os destinos que você já transportou anteriormente. Clique em um deles para mais informações</small>
+
+                            <div className="row destinos">
+                                {destinos.map((destino) => {
+                                    return(
+                                        <Accordion className="destino">
+                                            <AccordionSummary expandIcon={<MdOutlineKeyboardArrowDown />} className="resumo">
+                                                <p>
+                                                    <strong>Destino #{ ("00" + destino.id_destino).slice(-4) }: </strong>
+                                                    <small>{destino.endereco_destino}</small>
+                                                </p>
+                                            </AccordionSummary>
+
+                                            <AccordionDetails className="detalhes">
+                                                <p><strong>Endereço: </strong>{ destino.endereco_destino }</p>
+                                                <p><strong>Responsável: </strong>{ destino.responsavel_destino }</p>
+                                                <p><strong>Contato: </strong>{ destino.cel_destino }</p>
+                                                <p><strong>Quantidade de dependentes: </strong>{ destino.qtd_dependentes_destino }</p>
+
+                                                <button className="btn">Entrar em contato</button>
+                                            </AccordionDetails>
+                                        </Accordion>
+                                    )
+                                })}
+                            </div>
+
+                            <h3>Destinos disponíveis: </h3>
+                            <small>Esses são os disponíveis para receber os alimentos da nossa horta. Clique em um deles para mais informações e adicionar aos seus destinos.</small>
+
+                            <div className="row destinos">
+                                {destinos.map((destino) => {
+                                    return(
+                                        <Accordion className="destino">
+                                            <AccordionSummary expandIcon={<MdOutlineKeyboardArrowDown />} className="resumo">
+                                                <p>
+                                                    <strong>Destino #{ ("00" + destino.id_destino).slice(-4) }: </strong>
+                                                    <small>{destino.endereco_destino}</small>
+                                                </p>
+                                            </AccordionSummary>
+
+                                            <AccordionDetails className="detalhes">
+                                                {/* TODO: Adicionar link do maps: https://www.google.com/maps/search/Rua+Hip%C3%B3dromo,+720+CEP:+030.51-000 */}
+                                                <p><strong>Endereço: </strong>{ destino.endereco_destino }</p>
+                                                <p><strong>Responsável: </strong>{ destino.responsavel_destino }</p>
+                                                {/* TODO: Adicionar link de contato: https://wa.me/5511952526565?text=Oi%2C+tudo+bem%3F+Quando+posso+levar+os+alimentos+da+Ra%C3%ADzes+Solid%C3%A1rias+no+endere%C3%A7o+cadastrado%3F */}
+                                                <p><strong>Contato: </strong> <a href="">{ destino.cel_destino }</a></p>
+                                                <p><strong>Quantidade de dependentes: </strong>{ destino.qtd_dependentes_destino }</p>
+
+                                                <button className="btn">Adicionar aos meus destinos</button>
+                                            </AccordionDetails>
+                                        </Accordion>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )}
+
 
                 </div>
             </div>
